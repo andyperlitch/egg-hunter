d2e125659c90c6af8754bb43a7643318a553f51f
Andy Perlitch <aperlitch@sixfivelabs.com>
2020-01-24 00:36:55 -0800

    Updates

---
+++ precache-manifest.05c26a8bd074f163587821f3802f172e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99451d5fe61eaa72e9e954dc83b33a0c",
+    "url": "/egg-hunter/index.html"
+  },
+  {
+    "revision": "42587e3313ef43f1246e",
+    "url": "/egg-hunter/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2a4076255414fe63226d",
+    "url": "/egg-hunter/static/css/main.8f345eda.chunk.css"
+  },
+  {
+    "revision": "42587e3313ef43f1246e",
+    "url": "/egg-hunter/static/js/2.583d6989.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/egg-hunter/static/js/2.583d6989.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a4076255414fe63226d",
+    "url": "/egg-hunter/static/js/main.23b65ddb.chunk.js"
+  },
+  {
+    "revision": "2f15923a33a5d6fb5528",
+    "url": "/egg-hunter/static/js/runtime-main.bc5b2112.js"
+  },
+  {
+    "revision": "be280460ded23b41e70ba1c49453c923",
+    "url": "/egg-hunter/static/media/bunny.be280460.png"
+  },
+  {
+    "revision": "52ea1c7fe4ec64756c5fef8f5f82c380",
+    "url": "/egg-hunter/static/media/egg.52ea1c7f.png"
+  },
+  {
+    "revision": "71059532eefcdf100500f2c80121f043",
+    "url": "/egg-hunter/static/media/rock.71059532.png"
+  }
+]);
